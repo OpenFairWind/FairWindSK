2d0450f614f4685084b25a01750803702b6ee8eb
Raffaele Montella <raffaele.montella@uniparthenope.it>
2024-04-01 00:17:52 +0200

    Subscriprions now affect the signal k server (local document removed)

--- signalk/Subscription.cpp
+++ signalk/Subscription.cpp
@@ -1,25 +1,22 @@
 //
-// Created by Raffaele Montella on 01/03/22.
+// Created by Raffaele Montella on 31/03/24.
 //
 
-#include "Subscription.hpp"
-#include "../FairWindSK.hpp"
 
+
+#include "Subscription.hpp"
 
 namespace fairwindsk::signalk {
-/*
- * Subscription - Public Constructor
- */
-    Subscription::Subscription(const QString &fullPath, QObject *receiver, const char *member) {
+    Subscription::Subscription(const QString &context, const QString &path,
+                                             QObject *receiver, const char *member) {
 
-        auto fairWindSK = FairWindSK::getInstance();
-        auto signalK = fairWindSK->getSignalKDocument();
-        auto self = signalK->getSelf();
+        auto fullPath = context + "." + path;
 
-        QString fullPathEx = QString(fullPath).replace("${self}",self);
-        QString re = QString(fullPathEx).replace(".", "[.]").replace(":", "[:]").replace("*", ".*");
+        QString re = fullPath.replace(".", "[.]").replace(":", "[:]").replace("*", ".*");
+
         //qDebug() << "Subscription::Subscription re: " << re;
         regularExpression = QRegularExpression(re);
+
         this->receiver = receiver;
         memberName = QString(member);
 
@@ -27,32 +24,28 @@ namespace fairwindsk::signalk {
         int pos = memberName.lastIndexOf("::");
         memberName = memberName.right(memberName.length() - pos - 2);
 
-        // Get the initial value invoking the SignalK API
-        auto initialValue = signalK->get(fullPathEx);
-
-        // Inject the value in the local document
-        signalK->insert(fullPathEx,initialValue);
     }
 
-/*
- * ~Subscription - Destructor
- */
-    Subscription::~Subscription() = default;
+    Subscription::Subscription(const Subscription &other) {
+        this->regularExpression = other.regularExpression;
+        this->receiver = other.receiver;
+        this->memberName = other.memberName;
+    }
 
-/*
- * checkReceiver
- * Returns true is the provided receiver matches with the existent receiver in use
- */
     bool Subscription::checkReceiver(QObject *receiver) {
         if (this->receiver == receiver) return true;
         return false;
     }
 
-    bool Subscription::match(Document *document, const QString &fullPath) {
+    Subscription::~Subscription() = default;
+
+    bool Subscription::match(const QString &fullPath, const QJsonObject& updateObject) {
         //qDebug() << "Subscription::match : " << regularExpression << " ? " << fullPath;
+
         if (regularExpression.match(fullPath).hasMatch()) {
+
             //qDebug() << "Subscription::match : " << regularExpression << " = " << fullPath << " !!!";
-            QJsonObject updateObject = document->makeUpdate(fullPath);
+
             //qDebug() << "Subscription::match invokeMethod: " << memberName.toStdString().c_str();
             bool invokeResult = QMetaObject::invokeMethod(
                     receiver, memberName.toStdString().c_str(),
@@ -63,16 +56,4 @@ namespace fairwindsk::signalk {
 
         return false;
     }
-
-/*
- * Subscription - Public Constructor
- */
-    Subscription::Subscription(Subscription const &other) {
-        this->regularExpression = other.regularExpression;
-        this->receiver = other.receiver;
-        this->memberName = other.memberName;
-    }
 }
-
-
-
--- signalk/Subscription.hpp
+++ signalk/Subscription.hpp
@@ -1,31 +1,28 @@
 //
-// Created by Raffaele Montella on 01/03/22.
+// Created by Raffaele Montella on 31/03/24.
 //
 
-#ifndef FAIRWINDSK_SIGNALK_SUBSCRIPTION_HPP
-#define FAIRWINDSK_SIGNALK_SUBSCRIPTION_HPP
+#ifndef WEB_UI_JSON_SIGNALKSUBSCRIPTION_HPP
+#define WEB_UI_JSON_SIGNALKSUBSCRIPTION_HPP
 
+#include <QString>
 #include <QRegularExpression>
 #include <QJsonObject>
-#include "../FairWindSK.hpp"
-#include "Document.hpp"
 
 
 namespace fairwindsk::signalk {
+    class Subscription {
 
-    class Document;
-
-    class  Subscription {
     public:
         Subscription() = default;
 
-        Subscription(const QString &fullPath, QObject *receiver, const char *member);
+        Subscription(const QString &context, const QString &path, QObject *receiver, const char *member);
 
         Subscription(Subscription const &other);
 
         ~Subscription();
 
-        bool match(Document *document, const QString &fullPath);
+        bool match(const QString &fullPath, const QJsonObject& updateObject);
 
         bool checkReceiver(QObject *);
 
@@ -33,7 +30,8 @@ namespace fairwindsk::signalk {
         QRegularExpression regularExpression;
         QObject *receiver;
         QString memberName;
+
     };
 }
 
-#endif //FAIRWIND_SDK_SIGNALK_SUBSCRIPTION_HPP
+#endif //WEB_UI_JSON_SIGNALKSUBSCRIPTION_HPP
