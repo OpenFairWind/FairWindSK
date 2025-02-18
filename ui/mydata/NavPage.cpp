8b4fbcc5c9b0345eb3d1764ffab3af117c872e7f
ArjenR <arjenrr@gmail.com>
2025-02-18 14:44:46 +0000

    Fix name typos for NavPage

--- ui/mydata/NavPage.cpp
+++ ui/mydata/NavPage.cpp
@@ -5,7 +5,7 @@
 // You may need to build the project (run Qt uic code generator) to get "ui_Navpage.h" resolved
 
 #include "NavPage.hpp"
-#include "ui_Navpage.h"
+#include "ui_NavPage.h"
 
 #include <QColumnView>
 #include <QLayout>
